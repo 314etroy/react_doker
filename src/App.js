841723dee5e921080db4ed956e902831e2e31216
B <gtx7703d@gmail.com>
2022-04-06 19:58:17 +0300

    Docker Container Functionality for this project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>React 18</code>
+          <code>React 18</code>: I'm in a container?!?
         </p>
       </header>
     </div>
